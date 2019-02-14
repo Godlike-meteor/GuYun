29b4d6bcf756334809d84bd381881e1c6b3593f2
Godlike Meteor <1814533740@qq.com>
2019-02-14 11:41:27 +0800

    already init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 300ms点击延迟解决方案 npm install fastclick --save
+import fastClick from 'fastclick'
+// 重置项目中的CSS解决方案
+import './assets/styles/reset.css'
+// 1px 边框解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<!-- Home -->
+<template>
+  <div class="home">home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home {
+    font-size: 50px;
+  }
+</style>
